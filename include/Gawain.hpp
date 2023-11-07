1c9a25eb47ae4274a645e1e38f60462633a6b565
kingofknights <vikram.smvdu@gmail.com>
2023-11-07 21:47:43 +0530

    moved from LancelotAPI

---
+++ include/Gawain.hpp
@@ -0,0 +1,14 @@
+//
+// Created by kingofknights on 7/15/23.
+//
+
+#ifndef GAWAIN_INCLUDE_GAWAIN_GAWAIN_API_HPP_
+#define GAWAIN_INCLUDE_GAWAIN_GAWAIN_API_HPP_
+#pragma once
+
+#include "Gawain/API/Adaptor/Adaptor.hpp"
+#include "Gawain/API/Common/Common.hpp"
+#include "Gawain/API/Common/StockPacket.hpp"
+#include "Gawain/API/Strategy/Strategy.hpp"
+
+#endif	// GAWAIN_INCLUDE_GAWAIN_GAWAIN_API_HPP_

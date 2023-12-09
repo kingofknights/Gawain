6e258d79c3fd113e7d26ae12280c4b42b3d95f4f
kingofknights <vikram.smvdu@gmail.com>
2023-12-09 23:10:49 +0530

    inline some function

--- src/StockPacket.cpp
+++ src/StockPacket.cpp
@@ -13,65 +13,65 @@ namespace Lancelot::API {
 inline static int _globalStockPacketUniqueIdentifierCounter = 1;
 
 //--------------------------------------------------------------------------------------
-int Position::getLastTradeQuantity() const { return _lastTradeQuantity; }
-int Position::getLastTradePrice() const { return _lastTradePrice; }
-int Position::getTotalTradeQuantity() const { return _totalTradeQuantity; }
+inline constexpr int Position::getLastTradeQuantity() const { return _lastTradeQuantity; }
+inline constexpr int Position::getLastTradePrice() const { return _lastTradePrice; }
+inline constexpr int Position::getTotalTradeQuantity() const { return _totalTradeQuantity; }
 
-void Position::setLastTradeQuantity(int lastTradeQuantity_) { _lastTradeQuantity = lastTradeQuantity_; }
-void Position::setLastTradePrice(int lastTradePrice_) { _lastTradePrice = lastTradePrice_; }
-void Position::setTotalTradeQuantity(int totalTradeQuantity_) { _totalTradeQuantity = totalTradeQuantity_; }
+inline constexpr void Position::setLastTradeQuantity(int lastTradeQuantity_) { _lastTradeQuantity = lastTradeQuantity_; }
+inline constexpr void Position::setLastTradePrice(int lastTradePrice_) { _lastTradePrice = lastTradePrice_; }
+inline constexpr void Position::setTotalTradeQuantity(int totalTradeQuantity_) { _totalTradeQuantity = totalTradeQuantity_; }
 
 //--------------------------------------------------------------------------------------
 Internal::Internal() : _resultSetPtr(nullptr), _uniqueClassIdentity(++_globalStockPacketUniqueIdentifierCounter), _userAllocationPtr(nullptr) {}
 Internal::~Internal() { delete _userAllocationPtr; }
 
-int      Internal::getUniqueClassIdentity() const { return _uniqueClassIdentity; }
-uint32_t Internal::getStrategyNumber() const { return _strategyNumber; }
+inline constexpr int      Internal::getUniqueClassIdentity() const { return _uniqueClassIdentity; }
+inline constexpr uint32_t Internal::getStrategyNumber() const { return _strategyNumber; }
 
-ResultSetPtrT         Internal::getResultSetPtr() const { return _resultSetPtr; }
-const AdaptorPtrT&    Internal::getAdaptorPtr() const { return _adaptorPtr; }
-const StrategyPtrT&   Internal::getStrategyPtr() const { return _strategyPtr; }
-CustomUserAllocation* Internal::getUserAllocationPtr() const { return _userAllocationPtr; }
+inline ResultSetPtrT         Internal::getResultSetPtr() const { return _resultSetPtr; }
+inline const AdaptorPtrT&    Internal::getAdaptorPtr() const { return _adaptorPtr; }
+inline const StrategyPtrT&   Internal::getStrategyPtr() const { return _strategyPtr; }
+inline CustomUserAllocation* Internal::getUserAllocationPtr() const { return _userAllocationPtr; }
 
-void Internal::setStrategyNumber(uint32_t strategyNumber_) { _strategyNumber = strategyNumber_; }
-void Internal::setResultSetPtr(const ResultSetT* resultSetPtr_) { _resultSetPtr = resultSetPtr_; }
-void Internal::setAdaptorPtr(const AdaptorPtrT& adaptorPtr_) { _adaptorPtr = adaptorPtr_; }
-void Internal::setStrategyPtr(const StrategyPtrT& strategyPtr_) { _strategyPtr = strategyPtr_; }
-void Internal::setUserAllocationPtr(CustomUserAllocation* userAllocationPtr_) { _userAllocationPtr = userAllocationPtr_; }
+inline constexpr void Internal::setStrategyNumber(uint32_t strategyNumber_) { _strategyNumber = strategyNumber_; }
+inline constexpr void Internal::setResultSetPtr(const ResultSetT* resultSetPtr_) { _resultSetPtr = resultSetPtr_; }
+inline void           Internal::setAdaptorPtr(const AdaptorPtrT& adaptorPtr_) { _adaptorPtr = adaptorPtr_; }
+inline void           Internal::setStrategyPtr(const StrategyPtrT& strategyPtr_) { _strategyPtr = strategyPtr_; }
+inline constexpr void Internal::setUserAllocationPtr(CustomUserAllocation* userAllocationPtr_) { _userAllocationPtr = userAllocationPtr_; }
 
 //--------------------------------------------------------------------------------------
-bool     OrderDetails::isIoc() const { return _ioc; }
-Side     OrderDetails::getSide() const { return _side; }
-int      OrderDetails::getPrice() const { return _price; }
-uint32_t OrderDetails::getToken() const { return _token; }
-int      OrderDetails::getQuantity() const { return _quantity; }
-long     OrderDetails::getOrderNumber() const { return _orderNumber; }
+inline constexpr bool     OrderDetails::isIoc() const { return _ioc; }
+inline constexpr Side     OrderDetails::getSide() const { return _side; }
+inline constexpr int      OrderDetails::getPrice() const { return _price; }
+inline constexpr uint32_t OrderDetails::getToken() const { return _token; }
+inline constexpr int      OrderDetails::getQuantity() const { return _quantity; }
+inline constexpr long     OrderDetails::getOrderNumber() const { return _orderNumber; }
 
-const std::string& OrderDetails::getClientCode() const { return _clientCode; }
-const std::string& OrderDetails::getAlgoCode() const { return _algoCode; }
-const std::string& OrderDetails::getContractDescription() const { return _contractDescription; }
+inline std::string_view OrderDetails::getClientCode() const { return _clientCode; }
+inline std::string_view OrderDetails::getAlgoCode() const { return _algoCode; }
+inline std::string_view OrderDetails::getContractDescription() const { return _contractDescription; }
 
-void OrderDetails::setSide(Side side_) { _side = side_; }
-void OrderDetails::setIoc(bool ioc_) { _ioc = ioc_; }
-void OrderDetails::setToken(uint32_t token_) { _token = token_; }
-void OrderDetails::setPrice(int price_) { _price = price_; }
-void OrderDetails::setQuantity(int quantity_) { _quantity = quantity_; }
-void OrderDetails::setOrderNumber(long orderNumber_) { _orderNumber = orderNumber_; }
-void OrderDetails::setClientCode(const std::string& clientCode_) { _clientCode = clientCode_; }
-void OrderDetails::setAlgoCode(const std::string& algoCode_) { _algoCode = algoCode_; }
-void OrderDetails::setContractDescription(const std::string& contractDescription_) { _contractDescription = contractDescription_; }
+inline constexpr void OrderDetails::setSide(Side side_) { _side = side_; }
+inline constexpr void OrderDetails::setIoc(bool ioc_) { _ioc = ioc_; }
+inline constexpr void OrderDetails::setToken(uint32_t token_) { _token = token_; }
+inline constexpr void OrderDetails::setPrice(int price_) { _price = price_; }
+inline constexpr void OrderDetails::setQuantity(int quantity_) { _quantity = quantity_; }
+inline constexpr void OrderDetails::setOrderNumber(long orderNumber_) { _orderNumber = orderNumber_; }
+inline void           OrderDetails::setClientCode(const std::string& clientCode_) { _clientCode = clientCode_; }
+inline void           OrderDetails::setAlgoCode(const std::string& algoCode_) { _algoCode = algoCode_; }
+inline void           OrderDetails::setContractDescription(const std::string& contractDescription_) { _contractDescription = contractDescription_; }
 
 //--------------------------------------------------------------------------------------
 StockPacket::StockPacket() : _lastRequest(OrderRequest_NONE), _currentStatus(OrderStatus_NONE), _previousStatus(OrderStatus_NONE) {}
-void StockPacket::setLastRequest(OrderRequest lastRequest_) { _lastRequest = lastRequest_; }
-void StockPacket::setCurrentStatus(OrderStatus currentStatus_) { _currentStatus = currentStatus_; }
-void StockPacket::setPreviousStatus(OrderStatus previousStatus_) { _previousStatus = previousStatus_; }
+inline constexpr void StockPacket::setLastRequest(OrderRequest lastRequest_) { _lastRequest = lastRequest_; }
+inline constexpr void StockPacket::setCurrentStatus(OrderStatus currentStatus_) { _currentStatus = currentStatus_; }
+inline constexpr void StockPacket::setPreviousStatus(OrderStatus previousStatus_) { _previousStatus = previousStatus_; }
 
-OrderRequest StockPacket::getLastRequest() const { return _lastRequest; }
-OrderStatus  StockPacket::getCurrentStatus() const { return _currentStatus; }
-OrderStatus  StockPacket::getPreviousStatus() const { return _previousStatus; }
+inline constexpr OrderRequest StockPacket::getLastRequest() const { return _lastRequest; }
+inline constexpr OrderStatus  StockPacket::getCurrentStatus() const { return _currentStatus; }
+inline constexpr OrderStatus  StockPacket::getPreviousStatus() const { return _previousStatus; }
 
-bool StockPacket::execute(int price_, int quantity_, OrderRequest request_) {
+inline bool StockPacket::execute(int price_, int quantity_, OrderRequest request_) {
     if (not getAdaptorPtr()) {
         return false;
     }
@@ -88,7 +88,7 @@ bool StockPacket::execute(int price_, int quantity_, OrderRequest request_) {
     return false;
 }
 
-void StockPacket::executionReport(OrderStatus orderStatus_) {
+inline void StockPacket::executionReport(OrderStatus orderStatus_) {
     setPreviousStatus(getCurrentStatus());
     setCurrentStatus(orderStatus_);
     getStrategyPtr()->orderEventManager(getUniqueClassIdentity());
